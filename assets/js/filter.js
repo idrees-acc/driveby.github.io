770c96d2dace020c053e0417ab06d1754a18d9da
Idrees Tarwadi <idreestarwadi@Idreess-MacBook-Pro.local>
2022-12-15 21:50:29 +0530

    changes for animation

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,18 +1,16 @@
-
 $(".item_wrap").each(function () {
-  $(".item_wrap").addClass("d-none");
+  $(".item_wrap").addClass("d-none"); /* Hide all price list onload */
 });
 $(".tabs_wrap:first-child").each(function (e,d) {
-  console.log(e,d)
-  $(".item_list li:first-child").removeClass("d-none");
+  $(".item_list li:first-child").removeClass("d-none"); /* Show 1st price list onload */
 });
 const filterTabs = (tab) => {
-  $(".item_wrap").addClass("d-none");
-  $(".tabs_wrap li").removeClass("active");
-  $(`.tabs_wrap [data-tabs="${tab}"]`).addClass("active")
+  $(".item_wrap").addClass("d-none"); /* OnClick hide all price list */
+  $(".tabs_wrap li").removeClass("active"); /* Inactive all tabs */
+  $(`.tabs_wrap [data-tabs="${tab}"]`).addClass("active"); /* Active current tab */
   $(".tabs_wrap").each(function (e) {
-    $(`.item_wrap.${tab}`).removeClass("d-none").delay(0).queue(function (next) {
-      $(`.item_wrap.${tab} .container .row`).children(".col-lg-4").children(".card").toggleClass("animation")
+    $(`.item_wrap.${tab}`).removeClass("d-none").delay(0).queue(function (next) { /* OnClick show current tab price list */
+      $(`.item_wrap.${tab} .container .row`).children(".col-lg-4").children(".card").toggleClass("animation") /* Animation current tab price list with delay */
       next();
     });
   });
