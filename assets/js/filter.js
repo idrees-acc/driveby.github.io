33cef57ea52a9170dd1ee30d689c6a70bf980787
Idrees Tarwadi <Idrees Tarwadi>
2022-12-16 15:19:58 +0530

    header and animation with price list

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -3,8 +3,10 @@ $(".item_wrap").each(function () {
   $(".item_wrap").addClass("d-none");
 });
 $(".tabs_wrap:first-child").each(function (e,d) {
-  console.log(e,d)
-  $(".item_list li:first-child").removeClass("d-none");
+  $(".item_list li:first-child").removeClass("d-none").delay(0).queue(function (next) {
+    $(`.item_list li:first-child .container .row`).children(".col-lg-4").children(".card").toggleClass("animation")
+    next();
+  });
 });
 const filterTabs = (tab) => {
   $(".item_wrap").addClass("d-none");
